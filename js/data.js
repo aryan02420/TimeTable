d24e33cb9f98fac06e4b73e65e37324d1f90b2bb
Aryan <aryan@pop-os.localdomain>
2020-08-13 00:41:47 +0530

    initial commit

---
+++ js/data.js
@@ -0,0 +1,258 @@
+export const data = {
+	Math_F211 : {
+		Lectures : {
+			L1 : {
+				Teacher : "",
+				Room : "",
+				Time : [12,0,12,0,12,0]
+			},
+			L2 : {
+				Teacher : "",
+				Room : "",
+				Time : [12,0,12,0,12,0]
+			},
+			L3 : {
+				Teacher : "",
+				Room : "",
+				Time : [12,0,12,0,12,0]
+			},
+			L4 : {
+				Teacher : "",
+				Room : "",
+				Time : [16,0,16,0,16,0]
+			},
+			L5 : {
+				Teacher : "",
+				Room : "",
+				Time : [16,0,16,0,16,0]
+			},
+			L6 : {
+				Teacher : "",
+				Room : "",
+				Time : [16,0,16,0,16,0]
+			},
+			L7 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,9,0,9,0,9]
+			},
+			L8 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,9,0,9,0,9]
+			},
+			L9 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,9,0,9,0,9]
+			},
+			L10 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,12,0,12,0,12]
+			},
+			L11 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,12,0,12,0,12]
+			},
+			L12 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,12,0,12,0,12]
+			}
+		},
+		Tutorials : {
+			T1 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,0,17,0,0]
+			},
+			T2 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,17,0,0,0]
+			},
+			T3 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,17,0,0,0]
+			},
+			T4 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,0,17,0,0]
+			},
+			T5 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,0,14,0,0]
+			},
+			T6 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,17,0,0,0]
+			},
+			T7 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,0,17,0,0]
+			},
+			T8 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,17,0,0,0]
+			},
+			T9 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,0,17,0,0]
+			},
+			T10 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,17,0,0,0]
+			},
+			T11 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,0,17,0,0]
+			},
+			T12 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,17,0,0,0]
+			}
+		}
+	},
+	BITS_F225 : {
+		Lectures : {
+			L1 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,0,0,0,0]
+			}
+		}
+	},
+	CS_F213 : {
+		Lectures : {
+			L1 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,10,0,10,0,10]
+			}
+		},
+		Tutorials : {
+			T1 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,0,15,0,0]
+			}
+		}
+	},
+	CS_F214 : {
+		Lectures : {
+			L1 : {
+				Teacher : "",
+				Room : "",
+				Time : [10,0,10,0,10,0]
+			}
+		},
+		Tutorials : {
+			T1 : {
+				Teacher : "",
+				Room : "",
+				Time : [17,0,0,0,0,0]
+			}
+		}
+	},
+	CS_F215 : {
+		Lectures : {
+			L1 : {
+				Teacher : "",
+				Room : "",
+				Time : [12,0,12,0,12,0]
+			},
+			L2 : {
+				Teacher : "",
+				Room : "",
+				Time : [12,0,12,0,12,0]
+			},
+			L3 : {
+				Teacher : "",
+				Room : "",
+				Time : [15,0,15,0,15,0]
+			},
+			L4 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,12,0,12,0,12]
+			}
+		},
+		Tutorials : {
+			T1 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,0,14,0,0]
+			},
+			T2 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,0,14,0,0]
+			},
+			T3 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,0,14,0,0]
+			},
+			T4 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,0,14,0,0]
+			},
+			T5 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,0,14,0,0]
+			},
+			T6 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,0,14,0,0]
+			}
+		},
+		Practicals : {
+			P1 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,11,0,0,0,0]
+			},
+			P2 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,14,0,0,0,0]
+			},
+			P3 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,0,0,11,0]
+			}
+		},
+	},
+	CS_F222 : {
+		Lectures : {
+			L1 : {
+				Teacher : "",
+				Room : "",
+				Time : [9,0,9,0,9,0]
+			}
+		},
+		Tutorials : {
+			T1 : {
+				Teacher : "",
+				Room : "",
+				Time : [0,0,0,17,0,0]
+			}
+		}
+	}
+};
---
+++ js/index.js
@@ -0,0 +1,54 @@
+import {
+  data
+} from '/js/data.js';
+const courses = Object.keys(data);
+const container = document.querySelector('form');
+courses.forEach(course => {
+  const form = document.createElement('div');
+  const title = document.createElement('h3');
+  title.innerText = course;
+  form.appendChild(title);
+  const units = Object.keys(data[course]);
+
+  units.forEach(unit => {
+    const container2 = document.createElement('div');
+    const subtitle = document.createElement('h4');
+    subtitle.innerText = unit;
+    container2.appendChild(subtitle);
+
+    const sections = Object.keys(data[course][unit]);
+    sections.forEach(section => {
+      container2.innerHTML += `<input type="radio" id="${course}-${unit}-${section}" name="${course}-${unit}" value="${course}-${unit}-${section}"> <label for="${course}-${unit}-${section}">${section} - ${data[course][unit][section]["Teacher"]}</label><br>`
+    });
+    form.appendChild(container2);
+  });
+
+  container.appendChild(form);
+});
+
+
+const updateTT = () => {
+  const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+  const gridData = document.querySelectorAll('.data');
+  for (const slot of gridData) {
+    slot.innerHTML = '';
+  };
+  console.clear();
+  const formData = new FormData(container);
+  for (var value of formData.values()) {
+    const keys = value.split('-');
+    const sch = data[keys[0]][keys[1]][keys[2]]
+    for (let i = 0; i < 6; i++) {
+      if (sch['Time'][i] !== 0) {
+        const slot = document.querySelector(`.grid-area-${days[i]}${sch['Time'][i] - 7}`);
+        slot.innerHTML += `${keys[0]} ${keys[2]}`
+      }
+    }
+  };
+};
+
+container.addEventListener('change', () => {
+  updateTT();
+});
+
+updateTT();
