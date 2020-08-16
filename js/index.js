97384fbe5c6cfedb735e939e9f3d3810207e461b
Aryan <aryan@pop-os.localdomain>
2020-08-16 22:24:17 +0530

    added search

--- js/index.js
+++ js/index.js
@@ -10,6 +10,10 @@ const form2 = document.querySelector('#form2');
 const form1title = document.createElement('h3');
 form1title.innerText = 'Choose Courses';
 form1.appendChild(form1title);
+const searchbox = document.createElement('div');
+searchbox.id = 'search';
+searchbox.setAttribute('contenteditable', '');
+form1.appendChild(searchbox);
 const form1choices = document.createElement('div');
 courses.forEach(course => {
   form1choices.innerHTML += `<input type="checkbox" id="${course}" name="course" value="${course}"><label for="${course}">${course} - ${fullname[course] || ""}</label><br>`;
@@ -22,6 +26,19 @@ form1.addEventListener('change', () => {
 
 form2.addEventListener('change', () => {
   updateTT();
+});
+
+
+searchbox.addEventListener('keyup', () => {
+  const regex = RegExp(searchbox.innerText, 'i');
+  courses.forEach(course => {
+    let elem = document.querySelector(`#${course}`);
+    if (regex.test(`${course} - ${fullname[course] || ""}`)) {
+      elem.classList.remove('hidden');
+    } else {
+      elem.classList.add('hidden');
+    }
+  });
 });
 
 let selectedcourses;
@@ -64,6 +81,8 @@ const displayChoices = () => {
       const inputDiv = document.createElement('div');
       inputDiv.classList.add("inputDiv");
 
+      inputDiv.innerHTML += `<input type="radio" id="${course}-${unit}-none" name="${course}-${unit}" value="${course}-${unit}-none"> <label for="${course}-${unit}-none">None</label><br>`
+
       sections.forEach(section => {
 
         inputDiv.innerHTML += `<input type="radio" id="${course}-${unit}-${section}" name="${course}-${unit}" value="${course}-${unit}-${section}"> <label for="${course}-${unit}-${section}">${section} - ${data[course][unit][section]["Teacher"]}</label><br>`
@@ -100,6 +119,7 @@ const updateTT = () => {
   for (const value of form2Data.values()) {
 
     const keys = value.split('-');
+    if (keys[2] === 'none') continue;
     const sch = data[keys[0]][keys[1]][keys[2]];
 
     for (let i = 0; i < 6; i++) {
